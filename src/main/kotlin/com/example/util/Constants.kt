18f4a99b9852a05da3dac1812fdcc1b5a534bbd2
Stevdza-San <stefan.jovanavich@gmail.com>
2022-03-14 13:29:19 +0100

    Project Update

--- src/main/kotlin/com/example/util/Constants.kt
+++ src/main/kotlin/com/example/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.util
 
 object Constants {
 
-    const val AUDIENCE = "271505362417-u7oaq44q3o4uo8ritpajn32cinl3pa8a.apps.googleusercontent.com"
+    const val AUDIENCE = "YOUR_AUDIENCE"
     const val ISSUER = "https://accounts.google.com"
 
     const val DATABASE_NAME = "user_database"
